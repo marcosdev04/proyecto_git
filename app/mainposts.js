acd7741a60abb3f317a9735b55415d4acbe66b11
Marcos <marcos.mfd04@gmail.com>
2018-11-26 21:41:40 -0400

    ultima actualizacion blog api

--- app/mainposts.js
+++ app/mainposts.js
@@ -1,6 +1,9 @@
 function cargarPosts(){
+     
     
      let Liposts =  document.querySelector('#postusers');
+     Liposts.innerHTML="";
+     
      let dato = JSON.parse(localStorage.getItem('Datos'));
      let token = dato.token;
 
@@ -16,8 +19,8 @@ function cargarPosts(){
             let respuesta = JSON.stringify(response);
             
             response.forEach(post => {
-                let {title, tags,userEmail,userName,createdAt,likes,body,views,comments}=post;
-                 console.log(post);
+                let {id,title, tags,userEmail,userName,createdAt,likes,body,views,comments,liked}=post;
+                 
                  
                Liposts.innerHTML +=`
                <div class="row border">
@@ -37,8 +40,8 @@ function cargarPosts(){
                     </div>
                     <div class="w-100"></div>
                     <div class="col-lg-12 justify-self-end prueba">
-            <p class="text-right"><span class="comments"><img src="assets/img/comments.png" class="size">${comments}</span><img src="assets/img/views.png" class="size"> <span class="imgviews">${views}</span><a href="#" id="liked"><img src="assets/img/estrella.png" class="lik size"></a>${likes}</img></p>
-                    </div>
+                    <p class="text-right"><span class="comments"><img src="assets/img/comments.png" class="size">${comments}</span><img src="assets/img/views.png" class="size"> <span class="imgviews">${views}</span><button onclick="actulikes(${id},${liked})"></i><i class="${(liked) ? "far fa-star" : "fas fa-star"}"></i></button>${likes}</p>
+                    </div>                    
                </div>
                <br>
           `;
@@ -47,7 +50,26 @@ function cargarPosts(){
      
 }
 
+function actulikes(id,liked){
+     console.log(liked);
+     
+     let dato = JSON.parse(localStorage.getItem('Datos'));
+     let token = dato.token;
 
+     fetch(`http://68.183.27.173:8080/post/${id}/like`, {
+          method: (liked == true) ? 'DELETE' : 'PUT', // or 'PUT',         
+          headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        .then(function(e) {
+          // location.href="post_list.html";
+          cargarPosts();
+        })
+        .catch(error => console.error('Error:', error));
+     
+}
 
 $(document).ready(function(){
          cargarPosts();
